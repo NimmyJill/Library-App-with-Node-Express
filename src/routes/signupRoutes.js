7c1b4bf224a21b2fe5fa4ba975480e137c9df80a
Nimmy Jill <nimmisarah@gmail.com>
2020-06-20 10:48:15 +0530

    commit

--- src/routes/signupRoutes.js
+++ src/routes/signupRoutes.js
@@ -4,13 +4,7 @@ const signupRouter = express.Router();
 function router(nav) {
   signupRouter.get("/", function (req, res) {
     res.render("signup", {
-      nav: [
-        { link: "/books", name: "Books" },
-        { link: "/authors", name: "Authors" },
-        { link: "/contact", name: "Contact" },
-        { link: "/signup", name: "Signup" },
-        { link: "/login", name: "Login" },
-      ],
+      nav,
       title: "Books:Library",
     });
   });
